69a2d9483d281bd54723f0f1fbe550d0619567f4
AdarshSingh1103 <adarshrajput1103@gmail.com>
2024-10-15 23:29:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navigation from "./components/navigation";
+import Hero from "./components/Hero";
+
+
+const App = ()=>{
+
+  return(
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div>
+  );
+
+}
+export default App;
